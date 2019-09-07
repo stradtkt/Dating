e2afa7ddef143877041c2545760ccd1a1512a5a8
Kevin <stradtkt22@gmail.com>
2019-09-07 03:24:07 -0400

    Added the angular frontend

--- Dating/Controllers/ValuesController.cs
+++ Dating/Controllers/ValuesController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dating.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dating.Controllers
 {
@@ -10,36 +12,37 @@ namespace Dating.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<IActionResult> GetValues()
         {
-            return new string[] {"value1", "value2"};
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
         }
-
-        // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            return "value";
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
         }
-
-        // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            
         }
-
-        // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+            
         }
-
-        // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            
         }
     }
 }
