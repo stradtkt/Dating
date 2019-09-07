3f574b3df6363c3b44b208d55a99b9a5ea076033
Kevin <stradtkt22@gmail.com>
2019-09-07 16:56:18 -0400

    Added a method in the values.ts

--- Dating/DatingFrontEnd/src/app/app.module.ts
+++ Dating/DatingFrontEnd/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ValueComponent } from './components/value/value.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ValueComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
