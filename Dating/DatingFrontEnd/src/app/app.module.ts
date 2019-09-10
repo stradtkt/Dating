c7f65a17e25b821f3505afbe74cc00a61473d1ef
Kevin <stradtkt22@gmail.com>
2019-09-10 00:12:21 -0400

    Added a form to the navbar

--- Dating/DatingFrontEnd/src/app/app.module.ts
+++ Dating/DatingFrontEnd/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './components/value/value.component';
 import {HttpClientModule} from "@angular/common/http";
+import { NavComponent } from './components/nav/nav.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ValueComponent
+    ValueComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
