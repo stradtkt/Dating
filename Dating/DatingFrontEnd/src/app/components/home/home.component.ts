34ebc1a52745d949add5531b07d10911f046e156
Kevin <stradtkt22@gmail.com>
2019-09-10 15:11:17 -0400

    Added error handling

--- Dating/DatingFrontEnd/src/app/components/home/home.component.ts
+++ Dating/DatingFrontEnd/src/app/components/home/home.component.ts
@@ -12,20 +12,10 @@ export class HomeComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.getValues();
   }
 
   registerToggle() {
     this.registerMode = true;
-  }
-
-  getValues() {
-    this.http.get('http://localhost:5000/api/values')
-      .subscribe(res => {
-        this.values = res;
-      }, error => {
-        console.log(error);
-      });
   }
 
   cancelRegisterMode(registerMode: boolean) {
--- Dating/DatingFrontEnd/src/app/components/register/register.component.ts
+++ Dating/DatingFrontEnd/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-register',
@@ -6,16 +7,19 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
   register() {
-
+    this.authService.register(this.model).subscribe(() => {
+      console.log('registration successful');
+    }, error => {
+      console.log('There was an error');
+    });
   }
   cancel() {
     this.cancelRegister.emit(false);
