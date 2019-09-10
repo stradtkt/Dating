f337c4114eab1d15e156197c3b17a3b4089fc931
Kevin <stradtkt22@gmail.com>
2019-09-10 01:39:06 -0400

    Created the register and the home component

--- Dating/DatingFrontEnd/src/app/app.module.ts
+++ Dating/DatingFrontEnd/src/app/app.module.ts
@@ -7,12 +7,16 @@ import {HttpClientModule} from "@angular/common/http";
 import { NavComponent } from './components/nav/nav.component';
 import {FormsModule} from "@angular/forms";
 import {AuthService} from "./services/auth.service";
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ValueComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
