708e348a58ccf2db3b1a70c368b7360d6bffaf90
Kevin <stradtkt22@gmail.com>
2019-09-10 00:43:46 -0400

    Created a couple of methods in the nav component for people that are logged in

--- Dating/DatingFrontEnd/src/app/components/nav/nav.component.ts
+++ Dating/DatingFrontEnd/src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-nav',
@@ -8,13 +9,27 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   login() {
-    console.log("Your are now logged in");
+    this.authService.login(this.model)
+      .subscribe(next => {
+        console.log('Logged in successfully');
+      }, error => {
+        console.log('There was an error');
+      });
+  }
+  loggedin() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('Logged out');
   }
 
 }
